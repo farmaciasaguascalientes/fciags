50c854eb058e9262a0a0fc28b46e7c869099515c
diego <diego@tekniu.mx>
2022-02-15 11:11:39 -0600

    fix: check price

--- modulos/tis_price_checker_kiosk_sp/models/product_details.py
+++ modulos/tis_price_checker_kiosk_sp/models/product_details.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
 # - © Technaureus Info Solutions Pvt. Ltd 2020. All rights reserved.
-
 from odoo import api, fields, models
 
 
@@ -23,10 +22,16 @@ class ProductProduct(models.Model):
             uom_id = False
         tax_amount = 0
         get_price_tax = 0
-        regular_price = 0
+        get_price_regular = 0
+        get_price_off = 0
+        min_quantity = 0
         if product_details.id and price_list:
-            get_price = self.env['product.pricelist'].search([('id', '=', price_list)]).get_product_price(
+            get_price_off = self.env['product.pricelist'].search([('id', '=', price_list)]).get_product_price_rule(
+                product_details, 9999999, False)
+            get_price_regular = self.env['product.pricelist'].search([('id', '=', price_list)]).get_product_price(
                 product_details, 1, False)
+            min_quantity = self.env['product.pricelist.item'].browse(get_price_off[1]).min_quantity
+            get_price_off = get_price_off[0]
             if product_details.taxes_id:
                 for tax in product_details.taxes_id:
                     if not tax.price_include:
@@ -36,9 +41,13 @@ class ProductProduct(models.Model):
                         if tax.amount_type == 'percent':
                             tax_amount += (product_details.lst_price / 100) * tax.amount
                             get_price_tax += (get_price / 100) * tax.amount
-            regular_price = product_details.lst_price + tax_amount
-            get_price = get_price + get_price_tax
+                    else:
+                        tax_amount = tax.amount
+                        get_price_tax = tax_amount
+            get_price_regular = round(get_price_regular + tax_amount, 2)
+            get_price_off = round(get_price_off + get_price_tax, 2)
+
         else:
             get_price = product_details.list_price
-        return product_details.id, product_details.name, regular_price, product_details.barcode, \
-               product_details.default_code, get_price, product_details.currency_id.symbol, uom_id
+        return product_details.id, product_details.name, get_price_regular, product_details.barcode, \
+               product_details.default_code, get_price_off, product_details.currency_id.symbol, min_quantity
